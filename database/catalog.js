15cf5398602d8334e5d7b7e69f91f2794c3bc5f3
Phu Nguyen Duong Thanh <1234hdpa@gmail.com>
2026-02-13 14:17:30 +0700

    Update Button and feedback.

--- database/catalog.js
+++ database/catalog.js
@@ -30,7 +30,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260210-185456.md",
     "createdAt": "2026-02-10T18:54:56.3998700+07:00",
-    "updatedAt": "2026-02-13T04:44:32.546Z",
+    "updatedAt": "2026-02-13T07:11:48.373Z",
     "storyText": "**Here is a doodle for 07/2024!**\n\n**Thanks for all your support!**\n\n📔 🖌️ 🖊️\n\n\nGrove, a blue dragon warrior with a fervor for adventure, found himself walking through the forest towards a large lake. \"This is where we need to find the ingredients for the commissioner in the village,\" he thought to himself. \"It's odd that these ingredients are easily found elsewhere, yet he insists on collecting them from this lake.\"\n\n\nThe air around the lake was murky, thick with fog that obscured his vision. The fog was so dense that it was impossible to see the other edge of the lake. This mission, which seemed simple at first, was proving to be more challenging than Grove had anticipated. The mist-covered vision and the eerie, dark, and quiet space were enough to make even the bravest of adventurers hesitate.\n\n\nBut Grove was different. He had been through many incidents and strange adventures that had honed his courage and calmness. After surveying the lake for a while, he noticed strange flowers floating on the water's surface. They had dark green petals and bright pink stamens, a sight that Grove had never encountered before. His instincts and experience told him it was best not to come into contact with them.\n\n\nUsing a dry tree branch by the lake, Grove tested the depth of the water. It seemed that the edge of the lake was quite shallow, with the accumulated mud solid enough to step on. With newfound determination, Grove decided to venture down to the shallow beach near the shore to find the materials he needed for the commissioner's request.\n\n\nGrove, having left his heavy luggage and armor on the shore, ventured cautiously into the shallow mud with only his knife, searching for materials. The day seemed fortunate when he quickly discovered a bush of raw materials just a few dozen steps away. However, as he groped his way towards it, his feet sank into something soft beneath the surface.\n\n\nSuddenly, something erupted from the water with immense force, knocking Grove off his feet and sending him flying. Before he could even regain his bearings, a tangle of shadowy tentacles emerged from below, ensnaring his body. Desperate to escape, Grove struggled with all his might, but in the process, he lost his grip on his precious knife - the only tool that could have freed him. The danger was closing in, and Grove was running out of options.\n\n\nIn an instant, the tentacles coiled around Grove's legs, pulling him down into the glistening pink pistil. As he gazed upon his predicament, he saw his legs being drawn deeper into the shiny pink stamens, surrounded by equally lustrous dark green leaves. More tentacles emerged from the pistil, wrapping tightly around Grove's arms, rendering him helpless as he struggled in vain.\n\n\nA layer of liquid rubber oozed from the inner walls of the pistil, coating Grove's legs and gradually spreading up his body. As the rubber encased his lower body, the pistil shell also began to climb, starting from his knees, then his thighs, and eventually swallowing his waist and chest.\n\n\nOne tentacle forced its way into Grove's mouth, stifling his cries for help, and delved deeper into his throat, pumping a waxy, sweet-smelling pink rubbery liquid that tasted like marshmallows and distracted him from his plight. Meanwhile, another tentacle emitted hypnotic sounds and streams of magical light, gradually lulling Grove into a trance-like state and causing him to surrender to the tentacles' embrace.\n\n\nGrove's body was quickly enveloped in a layer of thick, green rubber, stretching from his head to his toes. The rubber around his head turned a vivid pink, and dark green leaves sprouted around his neck. A rubber tube connected his groin to his mouth, vibrating rhythmically and pumping a sweet, intoxicating liquid into his mouth. The layer of tentacles inside the plant's pistil teased him relentlessly, keeping him in a state of constant arousal.\n\n\nGrove couldn't control himself, and his body convulsed with pleasure as he ejaculated repeatedly. His head began to bulge and round, his features and horns gradually disappearing into the large, rubbery sphere. He was completely at the mercy of the rubber plant, his movements, breathing, and nutrition all controlled by his captor. No matter how hard he struggled, the rubber layer only grew thicker, and his struggles became weaker and weaker.\n\n\nAs Grove's resistance faded, the rubber tree expanded its chamber beneath the water's surface. His body was slowly dragged down into the mud, and as his head touched the water, the surrounding leaves closed tightly around him, sinking deep into the mud below. Grove was trapped forever, his body feeding the tree, and no one would ever find him. The rubber plant had claimed its victim, and Grove was lost to the darkness beneath the lake."
   },
   {
@@ -51,7 +51,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260211-001017.md",
     "createdAt": "2026-02-10T17:10:17.861Z",
-    "updatedAt": "2026-02-13T04:44:32.546Z",
+    "updatedAt": "2026-02-13T07:11:28.831Z",
     "storyText": "Suitable for drones in the pandemic."
   },
   {
@@ -75,7 +75,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260211-001405.md",
     "createdAt": "2026-02-10T17:14:05.747Z",
-    "updatedAt": "2026-02-13T04:44:32.546Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "Summertime, join in the beach party now :D"
   },
   {
@@ -98,7 +98,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260211-113938.md",
     "createdAt": "2026-02-11T04:39:38.195Z",
-    "updatedAt": "2026-02-13T04:44:32.546Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "You need a mask and a collar to go out, pup!"
   },
   {
@@ -119,7 +119,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260211-142057.md",
     "createdAt": "2026-02-11T07:20:57.786Z",
-    "updatedAt": "2026-02-13T04:44:32.546Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "Doodle for drakonika (https://twitter.com/drakonika123)"
   },
   {
@@ -141,7 +141,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260211-143637.md",
     "createdAt": "2026-02-11T07:36:37.976Z",
-    "updatedAt": "2026-02-13T04:44:32.547Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "Summer disaster = power cut + out of water"
   },
   {
@@ -161,7 +161,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260211-144945.md",
     "createdAt": "2026-02-11T07:49:45.544Z",
-    "updatedAt": "2026-02-13T04:44:32.547Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "Doodle for beautifulpanda20 (https://www.furaffinity.net/user/beautifulpanda20)"
   },
   {
@@ -184,7 +184,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260212-081642.md",
     "createdAt": "2026-02-12T01:16:42.777Z",
-    "updatedAt": "2026-02-13T04:44:32.547Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "He ready for a walk!"
   },
   {
@@ -205,7 +205,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260212-082326.md",
     "createdAt": "2026-02-12T01:23:26.567Z",
-    "updatedAt": "2026-02-13T04:44:32.547Z",
+    "updatedAt": "2026-02-13T07:11:28.832Z",
     "storyText": "Doodle for jikerdor (https://www.furaffinity.net/user/jikerdor)"
   },
   {
@@ -226,7 +226,7 @@ window.REPORT_CATALOG = [
     ],
     "story": "story/r20260212-083042.md",
     "createdAt": "2026-02-12T01:30:42.072Z",
-    "updatedAt": "2026-02-13T04:44:32.547Z",
+    "updatedAt": "2026-02-13T07:11:28.833Z",
     "storyText": "A doodle for my doggy friend."
   }
 ];
